559d38d15618e1c55af58744f99dd1e0d51955a2
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-04 12:12:21 -0500

    created a div that changes color when clicked

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [color, setColor] = useState('black');
 
-  const incrementCount = () => {
-    setCount(count + 1);
-  }
+  useEffect(() => {
+    const changeColorOnClick = () => {
+      if(color === 'black') {
+        setColor('red');
+      } else {
+        setColor('black');
+      }
+    };
 
-  const decrementCount = () => {
-    setCount(count - 1);
-  }
+    document.addEventListener('click', changeColorOnClick);
+
+    return () => {
+      document.removeEventListener('click', changeColorOnClick);
+    };
+  }, [color]);
 
   
 
   return (
-    <div className="App">
-      <div>{count}</div>
-      <button onClick={incrementCount}>increment</button>
-      <button onClick={decrementCount}>decrement</button>
+    <div>
+      <div
+        id="myDiv"
+        style={{
+          color: "white",
+          width: "100px",
+          height: "100px",
+          position: "absolute",
+          left: "50%",
+          top: "50%",
+          backgroundColor: color,
+        }}
+        >
+          this div can change color. Click on me!
+        </div>
     </div>
+    
   );
 }
 
