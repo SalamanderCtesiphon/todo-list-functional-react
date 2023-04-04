044bea9310829b9924e3cd0ef3aaf4a8dd328f63
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-04 13:52:25 -0500

    added the basic logic to create a task and then add it to the task array

--- src/App.js
+++ src/App.js
@@ -3,16 +3,39 @@ import Overview from './components/Overview'
 import './App.css'
 
 function App() {
+  const [text, setText] = useState('')
   const [tasks, setTasks ] = useState([])
 
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    if(!text) {
+      alert('Please add a task')
+      return
+    }
+    addTask({text})
+
+    setText('')
+  }
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task}
+    setTasks([...tasks, newTask])
+  }
   
 
   return (
     <div className="App">
+      <h1>To Do List</h1>
       <div>
-        <form>
-          <label></label>
-          <input/>
+        <form onSubmit={onSubmit}>
+          <input
+            type='text'
+            placeholder='Add Task'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
           <button>submit</button>
         </form>
       </div>
